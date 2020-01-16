3f24af5f642b5a8637d6d245ca79a6c61e3b1cce
Denis <denis.bivol@live.com>
2020-01-16 17:38:14 +0000

    login button added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
+import SignInSignOut from './pages/signin-signout/signin-signout';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInSignOut} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-signout/signin-signout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in'
+import './signin-signout.scss';
+
+const SignInSignOut = () => (
+    <div className='sign-in-sign-out'>
+        <SignIn />
+    </div>
+)
+
+export default SignInSignOut;
